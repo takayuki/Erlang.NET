d25276c35ad111aa9281edec7545e2626117ebb0
Takayuki Usui <usui@3di.jp>
2009-10-06 18:49:22 +0900

    experimental epmd as Windows Service

--- epmd/epmd.cs
+++ epmd/epmd.cs
@@ -17,24 +17,66 @@
  * %CopyrightEnd%
  */
 using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
 using Erlang.NET;
 using log4net;
 using log4net.Config;
 
 namespace Erlang.NET
 {
-    public class Epmd
+#if WIN32
+    public class Epmd : ServiceBase
     {
-	static Epmd()
-	{
-	    XmlConfigurator.Configure();
-	}
+        static Epmd()
+        {
+            XmlConfigurator.Configure();
+        }
 
-	public static void Main(string[] args)
-	{
-	    OtpEpmd epmd = new OtpEpmd();
-	    epmd.start();
-	    epmd.join();
-	}
+        private OtpEpmd m_epmd;
+        private bool m_started = false;
+
+        public Epmd()
+        {
+            base.AutoLog = false;
+            base.CanPauseAndContinue = false;
+            base.CanStop = true;
+            base.ServiceName = "Erlang Port Mapper Daemon";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (!m_started)
+            {
+                m_epmd = new OtpEpmd();
+                m_epmd.start();
+                m_started = true;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            if (m_started)
+            {
+                m_epmd.quit();
+                m_started = false;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            ServiceBase.Run(new Epmd());
+        }
     }
+
+    [RunInstaller(true)]
+    public class LoginServiceInstaller : Installer
+    {
+        public LoginServiceInstaller()
+        {
+            throw new NotSupportedException();
+        }
+    }
+#endif
 }
